1a40b60d8b9d6a4931d9692f6ba9babd43e4daaf
JhonataOliveiraTeixeira <jonasdeot@gmail.com>
2024-11-09 21:10:24 -0300

    Feature: Save in localstorge repose login

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -13,30 +13,29 @@ const register = (name, email, password) => {
 
 const login = (email, password) => {
   return axios
-    .post(API_URL + "signin", {
-      email,
-      password,
-    }, {
-      withCredentials: true // Garante o envio e recebimento de cookies
-    })
+    .post(
+      API_URL + "signin",
+      { email, password },
+      { withCredentials: true } // Garante o envio e recebimento de cookies
+    )
     .then((response) => {
-      if (response.data.email) {
-        // Armazenando o token e a mensagem no localStorage
-        localStorage.setItem("message", response.data); // Armazenando a mensagem de sucesso
+      if (response.data.message) {
+        // Armazena apenas a mensagem no localStorage
+        localStorage.setItem("message", response.data.message);
       }
-      console.log(response.data);
+      console.log( response.data.message);
       return response.data;
+    })
+    .catch((error) => {
+      console.error("Erro ao tentar login:", error);
+      throw error;
     });
 };
 
 const logout = async () => {
-  // Remove o token e a mensagem do localStorage
-  localStorage.removeItem("token");
-  localStorage.removeItem("email"); // Caso tenha armazenado mais dados
-  localStorage.removeItem("message"); // Removendo a mensagem também
-
+  localStorage.removeItem("message"); // Removendo a mensagem no logout
   try {
-    const response = await axios.post(API_URL + "signout");
+    const response = await axios.post(API_URL + "signout", {}, { withCredentials: true });
     return response.data;
   } catch (error) {
     console.error("Erro ao tentar logout:", error);
@@ -45,8 +44,8 @@ const logout = async () => {
 };
 
 const getCurrentUser = () => {
-  // Retorna o token armazenado no localStorage
-  return localStorage.getItem("token");
+  // Retorna o valor armazenado
+  return localStorage.getItem("message");
 };
 
 const AuthService = {
